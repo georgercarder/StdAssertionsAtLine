3e8785052822f754aec94c4aaf81b531ab4f6f42
George Carder <georgercarder@gmail.com>
2025-07-25 01:21:13 -0700

    cleanup

--- stdassert_at_line_preprocess.sh
+++ stdassert_at_line_preprocess.sh
@@ -6,12 +6,8 @@ echo "updating assert line number macros for" "$filepath"
 
 while IFS= read -r item
 do
-    #echo "og" "$item"
     lineNo=$(echo "$item" | awk '{print $1}' | sed -e s/://)
-    #echo "$lineNo"
 
     sed -i "$lineNo""s/assertEq[^,]*/assertEq(\"line:"$lineNo"\"/" "$filepath"
-
-    #echo "$lineReplaced"
 
 done < <(cat "$filepath" | grep "atLine\." -n)
